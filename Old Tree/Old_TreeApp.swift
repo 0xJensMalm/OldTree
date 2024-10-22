61cd80d06ae61c3b256994f0b9da1db10bc5beab
0xJensMalm <112858577+0xJensMalm@users.noreply.github.com>
2024-10-22 19:37:46 +0200

    Initial Commit

---
+++ Old Tree/Old_TreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Old_TreeApp.swift
+//  Old Tree
+//
+//  Created by Jens Aga Malm on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Old_TreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Old TreeTests/Old_TreeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Old_TreeTests.swift
+//  Old TreeTests
+//
+//  Created by Jens Aga Malm on 22/10/2024.
+//
+
+import Testing
+@testable import Old_Tree
+
+struct Old_TreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
